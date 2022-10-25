757b28abf323edc575be798f933e23d4ba1a1da6
MichalChoma <macawelly200@gmail.com>
2022-10-25 13:07:30 +0200

    create pages for ssg products and detail page for first 10 products from fakestoreapi

---
+++ pages/products.tsx
@@ -0,0 +1,58 @@
+import { InferGetStaticPropsType } from 'next'
+import { ProductListItem } from '../components/Product'
+import React from 'react'
+import Header from '../components/Header'
+import Main from '../components/Main'
+
+// komponent reactowy, dostaje propsy z getStaticProps, ale tylko wtedy gdy jest to oddzielna najwyzsza strona (krok 2)
+const Products = ({data} : InferGetStaticPropsType<typeof getStaticProps>) => { //ctrl + spacja ts podpowiedzi
+
+  return (
+    <>
+    <Header />
+    <Main>
+        {data.map((product) => (
+            <li key={product.id} className="shadow-xl border-2">
+            <ProductListItem data={{
+                id:product.id,
+                rating:product.rating.rate,
+                price:product.price,
+                count:product.rating.count,
+                thumbnailUrl: product.image,
+                title: product.title,
+            }}/>
+            </li>
+        ))}
+    </Main>
+    </>
+  )
+
+}
+
+
+// przed budowa strony (krok 1) ssg, docelowo tej funkcji nie bedzie w kodzie
+export const getStaticProps = async () => {
+    const res = await fetch(`https://fakestoreapi.com/products/`)
+    const data: StoreApiResponse[] = await res.json();
+
+    return {
+        props: {
+            data, //to bedzie przekazane do Products
+        }
+    }
+}
+
+export default Products
+
+interface StoreApiResponse {
+    id:number;
+    title:string;
+    price:number;
+    description:string;
+    category:string;
+    image:string;
+    rating:{
+        rate:number;
+        count:number;
+    }
+}
