5d4ba3f3b728cd4f9336d46a3a2f01aeec8b3b32
MichalChoma <macawelly200@gmail.com>
2022-10-25 13:14:59 +0200

    use Image component with next,add domains to next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  images:{
+    domains:['naszsklep-api.vercel.app', 'fakestoreapi.com', 'picsum.photos']
+  }
 }
 
 module.exports = nextConfig
